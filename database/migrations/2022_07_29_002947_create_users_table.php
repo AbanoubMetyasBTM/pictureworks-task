2b9fb3cf861168dc5de195d563e0c1a9147b5bee
abanoubmetyas <abanoub_b_565>
2022-07-29 02:43:28 +0200

    create user migration, user seeder, user factory, user model, remove unwanted migration files

---
+++ database/migrations/2022_07_29_002947_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->bigInteger('id', true)->unsigned();
+			$table->string('name')->nullable();
+			$table->text('comments')->nullable();
+		});
+
+        (new \Database\Seeders\UserSeeder())->run();
+
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
