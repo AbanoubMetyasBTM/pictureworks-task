7c832f6089c5f633d42c0bb702a42289fa798669
abanoubmetyas <abanoub_b_565>
2022-07-29 04:01:02 +0200

    add unit testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,39 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Tests\TestCase;
+
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+
+    public function test_getUserById()
     {
-        $this->assertTrue(true);
+        $userObj = User::getUserById(1);
+
+        $this->assertTrue(is_object($userObj));
     }
+
+    public function test_getUserByIdIsFalse()
+    {
+        $userObj = User::getUserById(10);
+
+        $this->assertTrue(!is_object($userObj));
+    }
+
+    public function test_updateUserComments()
+    {
+        $userObj    = User::getUserById(1);
+
+        $newComment = "new comment" . time();
+        $checkTrue  = User::updateUserComments($userObj, $newComment);
+
+        $userObj    = User::getUserById(1);
+
+        $this->assertTrue($checkTrue);
+        $this->assertStringContainsString($newComment, $userObj->comments);
+    }
+
+
 }
